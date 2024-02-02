484aca4d4c0fa4741c781b2d20982a5905ae26b7
eloytoman <83752779+eloytoman@users.noreply.github.com>
2024-02-02 15:52:37 +0100

    documentated functions, fix bug

--- tubular_simulations_source/model_execution.R
+++ tubular_simulations_source/model_execution.R
@@ -10,6 +10,16 @@ perform_simulations<-function(
     algorithm="static",
     return_params=FALSE
 ){
+  # Function that perform the simulation. Parameters:
+  #   - parameters. List with the necessary parameters, if it is not given, it uses
+  #     the parameters from the file 
+  #     "tubular_simulations_source/models/model_parameters.R"
+  #   - algorithm. String with the algorithm that we want to use. It can be 
+  #     whether "static" or "bending", or else it will fail.
+  #   - return_params. If it is set to TRUE, it returns a list with the 
+  #     results, the parameters, and the algorithm used. If FALSE (by default),
+  #     it will return just the results. It is convenient to set it to TRUE if 
+  #     we want to store the results.
   
   if (algorithm=="static") {
     if (is.null(parameters)) {
