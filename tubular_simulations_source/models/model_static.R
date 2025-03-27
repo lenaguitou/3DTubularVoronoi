27d37137ed888173e7b5af4547e6773822343aef
Léna Guitou <100426022+lenaguitou@users.noreply.github.com>
2025-03-27 10:41:11 +0100

    Add files via upload

--- tubular_simulations_source/models/model_static.R
+++ tubular_simulations_source/models/model_static.R
@@ -38,48 +38,77 @@ library(doParallel)
 #to see the functions in detail, see the N_cylinder_algorithm.R script
 #The functions here are the same, excepting that we need more parameters, since
 #we do the function call from another function (and so we don't define global
-# parameters)
+#parameters)
 #
 
-move_points<-function(pt,wid,len,rc,n_cells){
-  ind<-sample(1:n_cells,1)
+move_points<-function(pt,wid,len,rc,n){
+  ind<-sample(1:n,1)
   ptinx<-pt$x[ind]+rnorm(1,mean=0,sd=rc)
   ptiny<-pt$y[ind]+rnorm(1,mean=0,sd=rc)
   while((ptinx<0 || ptinx>wid)||(ptiny<0 || ptiny>len)){
     ptinx<-pt$x[ind]+rnorm(1,mean=0,sd=rc)
     ptiny<-pt$y[ind]+rnorm(1,mean=0,sd=rc)
   }
-  pt$x[c(ind,ind+n_cells,ind+2*n_cells)]<-c(ptinx,ptinx+wid,ptinx+2*wid)
-  pt$y[c(ind,ind+n_cells,ind+2*n_cells)]<-ptiny
+  pt$x[c(ind,ind+n,ind+2*n)]<-c(ptinx,ptinx+wid,ptinx+2*wid)
+  pt$y[c(ind,ind+n,ind+2*n)]<-ptiny
   return(pt)
 }
 
-tesellation_energy_N<-function(xt, yt, A0, rec, rad, gamad, lamad, n_cells, Layer, s0_ratio){
+tesellation_energy_N<-function(xt, yt, A0, rec, rad, gamad, lamad, n, Layer, s0){
   tesener<-sapply(1:Layer,function(i) {
     tesel<-deldir(xt*(rad[[i]]/rad[[1]]), yt, rw = rec[[i]])
-    tilest<-tile.list(tesel)[(n_cells+1):(2*n_cells)]
+    tilest<-tile.list(tesel)[(n+1):(2*n)]
     perims<-(tilePerim(tilest)$perimeters)/sqrt(A0)
     areas<-sapply(tilest,function(x){x$area})/A0
-    gam<-gamad*exp((1-(rad[[i]]/rad[[1]]))/s0_ratio)
+    gam<-gamad*exp((1-(rad[[i]]/rad[[1]]))/s0)
     sum((areas-1)^2+(gam/2)*(perims^2)+lamad*perims)
   })
   return(sum(tesener)/Layer)
 }
 
+tesellation_energy_N_parallel <- function(xt, yt, A0, rec, rad, gamad, lamad, n, Layer, s0) {
+  tesener <- unlist(mclapply(1:Layer, function(i) {
+    tesel <- deldir(xt * (rad[[i]] / rad[[1]]), yt, rw = rec[[i]])
+    tilest <- tile.list(tesel)[(n+1):(2*n)]
+    perims <- (tilePerim(tilest)$perimeters) / sqrt(A0)
+    areas <- sapply(tilest, function(x) {x$area}) / A0
+    gam <- gamad * exp((1 - (rad[[i]]/rad[[1]])) / s0)
+    sum((areas-1)^2 + (gam/2) * (perims^2) + lamad * perims)
+  }, mc.cores = detectCores() - 1))  # Use all but 1 core
+  return(sum(tesener) / Layer)
+}
+
+elastic_contractile_adhesion_energy_parallel <- function(xt, yt, A0, rec, rad, gamad, lamad, n_cells, Layer, s0_ratio) {
+energy_components <- do.call(rbind, mclapply(1:Layer, function(i) {
+  tesel <- deldir(xt * (rad[[i]] / rad[[1]]), yt, rw = rec[[i]])
+  tilest <- tile.list(tesel)[(n_cells + 1):(2 * n_cells)]
+  perims <- (tilePerim(tilest)$perimeters) / sqrt(A0)
+  areas <- sapply(tilest, function(x) { x$area }) / A0
+  gam <- gamad * exp((1 - (rad[[i]] / rad[[1]])) / s0_ratio)
+  # Compute energy components
+  elastic_energy <- sum((areas - 1)^2) / Layer
+  contractile_energy <- sum((gam / 2) * (perims^2)) / Layer
+  adhesion_energy <- sum(lamad * perims) / Layer
+  return(c(elastic_energy, contractile_energy, adhesion_energy))
+}, mc.cores = detectCores() - 1))  # Use multiple cores
+# Sum the components
+return(list(
+  ElasticEnergy = sum(energy_components[, 1]),
+  ContractileEnergy = sum(energy_components[, 2]),
+  AdhesionEnergy = sum(energy_components[, 3])
+))}
+
 elastic_contractile_adhesion_energy <- function(xt, yt, A0, rec, rad, gamad, lamad, n_cells, Layer, s0_ratio) {
-  
   energy_components <- sapply(1:Layer, function(i){
     tesel <- deldir(xt * (rad[[i]] / rad[[1]]), yt, rw = rec[[i]])
     tilest <- tile.list(tesel)[(n_cells + 1):(2 * n_cells)]
     perims <- (tilePerim(tilest)$perimeters) / sqrt(A0)
     areas <- sapply(tilest, function(x) { x$area }) / A0
     gam <- gamad * exp((1 - (rad[[i]] / rad[[1]])) / s0_ratio)
-    
     # Calculate energy components
     elastic_energy <- sum((areas - 1)^2)/Layer
     contractile_energy <- sum((gam / 2) * (perims^2))/Layer
     adhesion_energy <- sum(lamad * perims)/Layer
-    
     # Return the energy components
     return(c(elastic_energy, contractile_energy, adhesion_energy))
   })
@@ -207,24 +236,23 @@ metropolisad<-function(seed = 666, n_steps = 250, n_cells = 100, n_layers=5,
   rad <- numeric(n_layers)
   for(k in 1:n_layers){
     rad[[k]]<- apical_rad+(k-1)*(cyl_thickness/(n_layers-1)) #the radius of the layer k
-    rec[[k]]<-c(xmin,xmin+3*(2*pi*rad[[k]]),ymin,ymax)
+    rec[[k]]<- c(xmin,xmin+3*(2*pi*rad[[k]]),ymin,ymax)
   }
   
   #Start, first iteration
-  
   set.seed(seed)
   
   x1 <- runif(n_cells,xmin,xmax)
   y1 <- runif(n_cells,ymin,ymax)
-  x <-c(x1,x1+cyl_width_A,x1+2*cyl_width_A)
-  y <-c(y1,y1,y1)
+  x <- c(x1,x1+cyl_width_A,x1+2*cyl_width_A)
+  y <- c(y1,y1,y1)
   
-  points <- data.frame(x=x,y=y)
-  pointsinit <- points
-  energyinit <- tesellation_energy_N(points$x, points$y, A0 = Am, rec , rad,
-                                     gamad = gamma, lamad = lambda, n_cells, Layer = n_layers, s0_ratio = s0_ratio)
-  energiesinit <- elastic_contractile_adhesion_energy(points$x, points$y, A0 = Am, rec , rad,
-                                                      gamad = gamma, lamad = lambda, n_cells, Layer = n_layers, s0_ratio = s0_ratio)
+  pointsinit <- data.frame(x=x,y=y)
+  energyinit <- tesellation_energy_N(pointsinit$x, pointsinit$y, A0 = Am, rec = rec , rad =rad,
+                                     gamad = gamma, lamad = lambda, n = n_cells, Layer = n_layers, s0 = s0_ratio)
+  energiesinit <- elastic_contractile_adhesion_energy(pointsinit$x, pointsinit$y, A0 = Am, rec , rad,
+                                     gamad = gamma, lamad = lambda, n = n_cells, Layer = n_layers, s0_ratio = s0_ratio)
+  points <- pointsinit
   
   #We create the variables to store the results
   energhist <- data.frame(Iteration=numeric(n_steps), Total=numeric(n_steps),Elastic=numeric(n_steps),Contractile=numeric(n_steps),Adhesion=numeric(n_steps))
@@ -236,34 +264,31 @@ metropolisad<-function(seed = 666, n_steps = 250, n_cells = 100, n_layers=5,
                         y=numeric(3*n_cells*n_steps),
                         Iteration = integer(3*n_cells*n_steps))
   
-  histpts[1:(3*n_cells),c(1,2)] <- points
+  histpts[1:(3*n_cells),c(1,2)] <- pointsinit
   histpts[1:(3*n_cells),3] <- 0
-  points2 <- data.frame(x=x,y=y)
   
   energytesel <- energyinit
   
   #Start of the loop
   for (j in 1:n_steps) {
-    for(k in 1:n_cells) {
-      points2<-move_points(points,wid = cyl_width_A,len = cyl_length,rc = r,n_cells = n_cells)  
-      energytesel2<-tesellation_energy_N(points2$x, points2$y, A0 = Am, rec , rad,
-                                         gamad = gamma, lamad = lambda, n_cells, Layer = n_layers, s0_ratio = s0_ratio)
-      c<-choice_metropolis(energytesel2-energytesel,beta) 
+    print(paste0("Iteration ",j))
+    for(k in 1:100) { #to go faster
+      points2 <- move_points(points,wid = cyl_width_A,len = cyl_length,rc = r,n = n_cells)  
+      energytesel2 <- tesellation_energy_N(points2$x, points2$y, A0 = Am,rec = rec , rad =rad,
+                                         gamad = gamma, lamad = lambda, n = n_cells, Layer = n_layers, s0 = s0_ratio)
+      c <- choice_metropolis(energytesel2-energytesel,beta) 
       cond <- c==1
       if(cond){
-        points<-points2
+        points <- points2
         energytesel <- energytesel2
-        energiestesel <- elastic_contractile_adhesion_energy(points$x, points$y, A0 = Am, rec , rad,
-                                                             gamad = gamma, lamad = lambda, n_cells, Layer = n_layers, s0_ratio = s0_ratio)
-      }
-    }
+        energiestesel <- elastic_contractile_adhesion_energy(points$x, points$y, A0 = Am, rec = rec , rad = rad,
+                                                             gamad = gamma, lamad = lambda, n_cells = n_cells, Layer = n_layers, s0_ratio = s0_ratio)
+      }}
     gc()
     histpts[(j*3*n_cells+1):(j*3*n_cells+3*n_cells),c(1,2)] <- points
     histpts[(j*3*n_cells+1):(j*3*n_cells+3*n_cells),3]<-j
     energhist[j+1,c(1,2)] <- c(j,energytesel)
     energhist[j+1,c(3,4,5)] <- energiestesel
-
-    print(paste0("step ",j," done"))
   }
   # nu2 <- nu_sq(points = points, rec = rec, n_cells = 100)
   return(list(points_evolution=histpts 
